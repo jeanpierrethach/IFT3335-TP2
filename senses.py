6ad101820a303251ce09b939eced776db4ae0810
Thach Jean-Pierre <jeanpierre.thach@gmail.com>
2018-05-01 13:11:56 -0400

    added third context window

--- senses.py
+++ senses.py
@@ -36,51 +36,63 @@ with open(input_file) as f:
             if re.match("^interest[0-9AB]", p[0]):
                 prevword = None
                 prevword_2 = None
+                prevword_3 = None
                 nextword = None
                 nextword_2 = None
+                nextword_3 = None
                 prevtag = None
                 prevtag_2 = None
+                prevtag_3 = None
                 nexttag = None
                 nexttag_2 = None
+                nexttag_3 = None
 
-                for n in range(1,3):
+                for n in range(1,4):
                     i = idx+n
                     if i > len(pairs)-1:
                         continue
-                    
                     if n == 1:
                         nextword = pairs[i][0]
                         nexttag = pairs[i][1]
                     elif n == 2:
                         nextword_2 = pairs[i][0]
                         nexttag_2 = pairs[i][1]
-                for n in range(1,3):
+                    elif n == 3:
+                        nextword_3 = pairs[i][0]
+                        nexttag_3 = pairs[i][1]
+                for n in range(1,4):
                     j = idx-n
                     if j < 0:
                         continue
-                   
                     if n == 1:
                         prevword = pairs[j][0]
                         prevtag = pairs[j][1]
                     elif n == 2:
                         prevword_2 = pairs[j][0]
                         prevtag_2 = pairs[j][1]
+                    elif n == 3:
+                        prevword_3 = pairs[j][0]
+                        prevtag_3 = pairs[j][1]
 
-                recorded_words.append((prevword_2, prevword, nextword, nextword_2, prevtag_2, prevtag, nexttag, nexttag_2, p[0]))
+                recorded_words.append((prevword_3, prevword_2, prevword, nextword, nextword_2, nextword_3, prevtag_3, prevtag_2, prevtag, nexttag, nexttag_2, nexttag_3, p[0]))
             
 # Output .arff format
 with open(output_file, 'w') as f:
     f.write("% interestacl94.arff")
     f.write("\n\n")
     f.write("@relation interest\n\n")
+    f.write("@attribute prevword_3 String\n")
     f.write("@attribute prevword_2 String\n")
     f.write("@attribute prevword String\n")
     f.write("@attribute nextword String\n")
     f.write("@attribute nextword_2 String\n")
+    f.write("@attribute nextword_3 String\n")
+    f.write("@attribute prevtag_3 String\n")
     f.write("@attribute prevtag_2 String\n")
     f.write("@attribute prevtag String\n")
     f.write("@attribute nexttag String\n")
     f.write("@attribute nexttag_2 String\n")
+    f.write("@attribute nexttag_3 String\n")
     f.write("@attribute 'Class' {'interest1', 'interest2', 'interest3', 'interest4', 'interest5', 'interest6'}\n\n")
     
     f.write("@data\n")
